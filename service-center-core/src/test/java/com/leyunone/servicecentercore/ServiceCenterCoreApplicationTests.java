b8921ee826f15dfcb85e66035e15f252f56bc5fe
leyunone <365627310@qq.com>
2023-07-28 22:05:16 +0800

    Initial commit

---
+++ service-center-core/src/test/java/com/leyunone/servicecentercore/ServiceCenterCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyunone.servicecentercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCenterCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
